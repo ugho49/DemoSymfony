5ab1b00ca2cbd0b0028baa7e82c7b74ae724efd1
Ugho STEPHAN <stephan.ugho@gmail.com>
2017-07-19 15:35:25 +0200

    implement "update my profile"

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -9,9 +9,12 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\User;
+use AppBundle\Form\UserType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -23,12 +26,13 @@ class UserController extends Controller
     /**
      * Finds and displays a user entity.
      *
-     * @Route("/user", name="user_profile")
+     * @Route("/user-profile", name="user_profile")
      * @Method("GET")
      * @return Response
      */
     public function showAction()
     {
+        /** @var User $currentUser */
         $user = $this->get('security.token_storage')->getToken()->getUser();
 
         return $this->render('user/show.html.twig', array(
@@ -36,4 +40,39 @@ class UserController extends Controller
         ));
     }
 
+    /**
+     * Displays a form to edit an existing user entity.
+     *
+     * @Route("/user-profile/edit", name="user_profile_edit")
+     * @Method({"GET", "POST"})
+     * @param Request $request
+     * @return RedirectResponse|Response
+     */
+    public function editAction(Request $request)
+    {
+        /** @var User $currentUser */
+        $currentUser = $this->get('security.token_storage')->getToken()->getUser();
+
+        $em   = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository("AppBundle:User");
+        $user = $repo->find($currentUser->getId());
+
+        $editForm = $this->createForm(new UserType(), $user);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em->flush();
+
+            $request->getSession()
+                ->getFlashBag()
+                ->add('info', 'The user has been successfully updated');
+
+            return $this->redirectToRoute('user_profile');
+        }
+
+        return $this->render('user/edit.html.twig', array(
+            'user' => $user,
+            'edit_form' => $editForm->createView(),
+        ));
+    }
 }
---
+++ src/AppBundle/Form/UserType.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ustephan2016
+ * Date: 19/07/2017
+ * Time: 09:50
+ */
+
+namespace AppBundle\Form;
+
+use AdminBundle\Enum\RolesEnum;
+use AppBundle\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('firstname', TextType::class, array(
+                "attr" => array(
+                    "placeholder" => "firstname"
+                )
+            ))
+            ->add('lastname', TextType::class, array(
+                "attr" => array(
+                    "placeholder" => "lastname"
+                )
+            ))
+            ->add('email', EmailType::class, array(
+                "attr" => array(
+                    "placeholder" => "email"
+                )
+            ))
+            ->add('birthday', DateType::class, array(
+                'required' => false,
+                'widget' => 'single_text',
+                // render as type="date"
+                'html5' => true,
+            ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => User::class
+        ));
+    }
+}
